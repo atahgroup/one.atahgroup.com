6d77143ce5edd22667ea9f90afc94a6b5d0b61a8
Wesley Jones <wesjones2004@gmail.com>
2025-12-23 13:44:10 -0500

    Refactor Property layout: change to grid system for improved responsiveness

--- app/property/page.tsx
+++ app/property/page.tsx
@@ -3,11 +3,12 @@ import React from "react";
 
 export default function Property() {
   return (
-    <main className="min-h-screen flex flex-col xl:flex-row gap-2">
-      <div className="w-full flex bg-background">
+    <main className="min-h-screen grid grid-cols-1 xl:grid-cols-3 grid-rows-2 xl:grid-rows-1">
+      <div className="col-span-1 w-full">
+        <h1 className="text-2xl font-bold mx-4 mt-3 mb-1">Vending Machines</h1>
         <VendingMachineList />
       </div>
-      <div className="w-full flex bg-green-400">
+      <div className="col-span-2 w-full flex bg-green-400">
         {/* TODO: interactive 3d world map */}
       </div>
     </main>
